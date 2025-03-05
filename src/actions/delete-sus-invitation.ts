aaacf846bf04cd47816d172a01f49ea02414d535
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2025-03-05 18:23:30 +1300

    Add action for deleting sus invitation

---
+++ src/actions/delete-sus-invitation.ts
@@ -0,0 +1,62 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { auth } from "../auth";
+import prisma from "../db";
+
+
+export async function createSusInvitation(id: string) {
+    const session = await auth();
+
+    const userId = session?.user?.id;
+
+    if (!userId) throw new Error("User not found");
+
+    const invitation = await prisma.susInvitation.findUnique({
+        where: {
+            id,
+        },
+        include: {
+            susResponse: true,
+            campaign: {
+                select: {
+                    project: {
+                        select: {
+                            id: true,
+                            ownerId: true,
+                        }
+                    },
+                    susResponses: true,
+                }
+            }
+        }
+    });
+
+    if (!invitation) {
+        throw new Error("Invitation not found.");
+    }
+
+    if (invitation.campaign.project.ownerId !== userId) {
+        throw new Error("Unauthorized.");
+    }
+
+    if (invitation.susResponse) {
+        throw new Error("Invitation already has a response.");
+    }
+
+    try {
+        await prisma.susInvitation.delete({
+            where: {
+                id,
+            }
+        });
+
+        revalidatePath(`/projects/${invitation.campaign.project.id}/campaigns/${invitation.campaignId}`);
+    } catch (error) {
+        return {
+            errors: [
+                "An unexpected error occurred while tying to delete a sus invitation. Please try again.",
+            ],
+        };
+    }
+}
