0cd7281b3018049b1b5c84fbc0ceca85b886b759
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2025-03-15 14:14:25 +1300

    Set up mock user for tests

---
+++ prisma/seed.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      id: "bob",
+      name: "Bob",
+      email: "bob@prisma.io",
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async () => {
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,27 +1,57 @@
-import type { NextAuthConfig } from 'next-auth'
-import GitHubProvider from 'next-auth/providers/github'
+import type { NextAuthConfig } from "next-auth";
+import { Provider } from "next-auth/providers";
+import Credentials from "next-auth/providers/credentials";
+import GitHubProvider from "next-auth/providers/github";
+
+const providers: Provider[] = [
+  GitHubProvider({
+    clientId: process.env.GITHUB_ID || "",
+    clientSecret: process.env.GITHUB_SECRET || "",
+    allowDangerousEmailAccountLinking: true, // Resolves "To confirm your identity, sign in with the same account you used originally" error when signing in with GitHub
+  }),
+];
+
+if (process.env.NEXT_ENV === "test") {
+  providers.push(
+    Credentials({
+      id: "password",
+      name: "Password",
+      credentials: {
+        password: { label: "Password", type: "password" },
+      },
+      authorize: (credentials) => {
+        if (credentials.password === "password") {
+          // This Bob should be seeded in the database
+          return {
+            id: "bob",
+            email: "bob@prisma.io",
+            name: "Bob",
+          };
+        }
+        return null;
+      },
+    })
+  );
+}
 
 export default {
-    providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID || '',
-            clientSecret: process.env.GITHUB_SECRET || '',
-            allowDangerousEmailAccountLinking: true // Resolves "To confirm your identity, sign in with the same account you used originally" error when signing in with GitHub
-        })
-    ],
-    callbacks: {
-        authorized({ auth }) {
-            // Logged in users are authenticated, otherwise redirect to login page
-            return !!auth?.user
-        },
-        session: async ({ session, token }) => {
-            if (session.user && token.sub) {
-                session.user.id = token.sub
-            }
-            return session
-        },
+  providers,
+  callbacks: {
+    authorized({ auth }) {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth?.user;
     },
-    pages: {
-        signIn: '/auth/signin',
+    session: async ({ session, token }) => {
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+      }
+      return session;
     },
-} satisfies NextAuthConfig
+  },
+  pages:
+    process.env.NEXT_ENV === "test"
+      ? undefined
+      : {
+          signIn: "/auth/signin",
+        },
+} satisfies NextAuthConfig;
