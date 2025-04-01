da8898ab2727169f518cfc113f2fc7988be9096f
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2025-04-01 22:13:47 +1300

    Add public route for welcome page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,12 @@
-import NextAuth from 'next-auth'
+import NextAuth from "next-auth";
 
-import authConfig from './auth.config'
+import authConfig from "./auth.config";
 
-export const { auth: middleware } = NextAuth(authConfig)
+export const { auth: middleware } = NextAuth(authConfig);
 
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|projects).*)'],
-}
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|projects|welcome).*)",
+  ],
+};
