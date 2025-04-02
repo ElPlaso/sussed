19092a2aa06d2a8f0e5667e17a3d85813051ea35
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2025-04-03 00:11:58 +1300

    Revert middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ export const { auth: middleware } = NextAuth(authConfig);
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|public|favicon.ico|projects|welcome|examples).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|projects|welcome|examples).*)",
     "/projects/new",
   ],
 };
