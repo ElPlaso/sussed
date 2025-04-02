129a97c30cc9af3f069036f0c228e82634f8cc10
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2025-04-02 20:59:01 +1300

    Update new project page

--- src/app/(sussed)/projects/new/page.tsx
+++ src/app/(sussed)/projects/new/page.tsx
@@ -1,18 +1,40 @@
-import { auth } from "@/auth";
-import NewProjectWrapper from "@/components/projects/new/NewProjectWrapper";
+"use client";
 
-import { redirect } from "next/navigation";
+import { createProject } from "@/actions/create-project";
+import ProjectForm from "@/components/projects/ProjectForm";
+import { useSession } from "next-auth/react";
+import { redirect, useRouter } from "next/navigation";
 
-export default async function NewProjectPage() {
-  if (!(await auth())) {
+export default function NewProjectPage() {
+  const auth = useSession();
+
+  if (!auth) {
     redirect("/auth/signin");
   }
+
+  const router = useRouter();
+
+  const handleSubmit = async (formData: FormData) => {
+    const result = await createProject(formData);
+    if (!result?.errors) {
+      router.push("/");
+    }
+    return result;
+  };
+
+  const handleAction = (_prevState: unknown, formData: FormData) => {
+    return handleSubmit(formData);
+  };
+
+  const handleCancel = () => {
+    router.push("/");
+  };
 
   return (
     <main className="flex flex-col px-12 py-10 items-center">
       <div className="flex flex-col gap-y-8 w-2/3 max-lg:w-full">
         <h2 className="text-lg text-start">New Project</h2>
-        <NewProjectWrapper />
+        <ProjectForm onAction={handleAction} onCancel={handleCancel} />
       </div>
     </main>
   );
